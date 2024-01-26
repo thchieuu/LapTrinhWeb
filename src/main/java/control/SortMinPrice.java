2b7c5c288288264993fe5fe1800b58af769f5c34
Nhan2108cute <21130460@st.hcmuaf.edu.vn>
2024-01-26 21:51:08 +0700

    sort price and name

---
+++ src/main/java/control/SortMinPrice.java
@@ -0,0 +1,32 @@
+package control;
+
+import dao.DAO;
+import entity.Category;
+import entity.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "SortPriceMinToMax", value = "/SortPriceMinToMax")
+public class SortMinPrice extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String a= request.getParameter("filter");
+        DAO dao = new DAO();
+        List<Category> listC = dao.getAllCategory();
+        List<Product> FilterList = dao.getAllProductMinToMax();
+        request.setAttribute("listP", FilterList);
+        request.setAttribute("listC", listC);
+        request.getRequestDispatcher("list-product.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+doGet(request,response);
+    }
+}
