290320ac67ff63fbfb47e6e8e436ef8ef6440102
Nhan2108cute <21130460@st.hcmuaf.edu.vn>
2024-01-26 21:55:08 +0700

    gioi thieu

---
+++ src/main/java/control/FAQControl.java
@@ -0,0 +1,21 @@
+package control;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "faq", value = "/introduce")
+public class FAQControl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("FAQ.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
